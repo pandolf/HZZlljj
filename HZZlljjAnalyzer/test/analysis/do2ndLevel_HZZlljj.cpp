936c8572ced6d87c2bcc4b55f0871beb4ef89045
pandolf <>
2010-08-05 14:26:30 +0000

    added argc argv

--- HZZlljjAnalyzer/test/analysis/do2ndLevel_HZZlljj.cpp
+++ HZZlljjAnalyzer/test/analysis/do2ndLevel_HZZlljj.cpp
@@ -1,9 +1,18 @@
 #include "Ntp1Analyzer_HZZlljj.h"
+#include <stdlib.h>
 
 
-int main() {
 
-  Ntp1Analyzer_HZZlljj* na = new Ntp1Analyzer_HZZlljj("H130");
+int main( int argc, char* argv[]) {
+
+  if( argc < 2 ) {
+    std::cout << "USAGE: ./do2ndLevel_Hzzlljj [dataset]" << std::endl;
+    exit(31);
+  }
+
+  std::string dataset(argv[1]);
+
+  Ntp1Analyzer_HZZlljj* na = new Ntp1Analyzer_HZZlljj(dataset);
 
   na->LoadInput();
   na->Loop();
