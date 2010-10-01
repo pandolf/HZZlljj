49baf4ffb00b04496757dc97b099253761161d80
pandolf <>
2010-10-01 12:24:40 +0000

    added ttbar and ZZ cross-sections. added alpgen scaling to NNLO

--- HZZlljjAnalyzer/test/analysis/merge_and_setWeights.cpp
+++ HZZlljjAnalyzer/test/analysis/merge_and_setWeights.cpp
@@ -2,12 +2,15 @@
 #include "TChain.h"
 #include "TTree.h"
 #include "TH1F.h"
+#include "TRegexp.h"
+#include "TString.h"
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 
 
 TChain* tree = 0;
+std::string analysisType_;
 
 struct EventsAndLumi {
   int nTotalEvents;
@@ -22,12 +25,19 @@ float getWeight( const std::string& dataset, int nEvents );
 
 int main( int argc, char* argv[] ) {
 
-  if( argc!=2 ) {
-    std::cout << "USAGE: ./merge_and_setWeights [dataset]" << std::endl;
+  if( argc!=2 && argc!=3 ) {
+    std::cout << "USAGE: ./merge_and_setWeights [dataset] [analysisType=\"HZZlljj\"]" << std::endl;
     exit(917);
   }
 
   std::string dataset = argv[1];
+
+  analysisType_ = "HZZlljj";
+  if( argc==3 ) {
+    std::string analysisType_str(argv[2]);
+    analysisType_ = analysisType_str;
+  }
+
 
   tree = new TChain("reducedTree");
 
@@ -41,7 +51,7 @@ int main( int argc, char* argv[] ) {
   // and now set the weights
   tree->SetBranchStatus( "eventWeight", 0 );
   
-  std::string outfilename = "HZZlljj_2ndLevelTreeW_"+dataset+".root";
+  std::string outfilename = analysisType_ + "_2ndLevelTreeW_"+dataset+".root";
   TFile* outfile = new TFile(outfilename.c_str(), "recreate");
   outfile->cd();
 
@@ -77,7 +87,7 @@ int main( int argc, char* argv[] ) {
 
 EventsAndLumi addInput( const std::string& dataset ) {
 
-  std::string infileName = "files_HZZlljj_2ndLevel_" + dataset + ".txt";
+  std::string infileName = "files_"+analysisType_+"_2ndLevel_" + dataset + ".txt";
   TH1F* h1_lumi;
   TH1F* h1_nCounter;
 
@@ -88,7 +98,7 @@ EventsAndLumi addInput( const std::string& dataset ) {
   FILE* iff = fopen(infileName.c_str(),"r");
   if(iff == 0) {
     std::cout << "cannot open input file '" << infileName << "' ... adding single file." << std::endl;
-    infileName = "HZZlljj_2ndLevelTree_" + dataset + ".root";
+    infileName = analysisType_+"_2ndLevelTree_" + dataset + ".root";
     std::string treeName = infileName +"/reducedTree";
     tree->Add(treeName.c_str());
     std::cout << "-> Added " << treeName << ". Tree has " << tree->GetEntries() << " entries." << std::endl;
@@ -108,7 +118,7 @@ EventsAndLumi addInput( const std::string& dataset ) {
     }
     infile->Close();
 
-  } else {
+  } else { //if file is good:
 
     char singleLine[500];
 
@@ -123,14 +133,14 @@ EventsAndLumi addInput( const std::string& dataset ) {
       if( h1_nCounter!=0 ) {
         totalEvents += h1_nCounter->GetBinContent(1);
       } else {
-        std::cout << " WARNING! File '" << rootfilename << "' has no nCounter information. Skipping." << std::endl;
+        std::cout << std::endl << " WARNING! File '" << rootfilename << "' has no nCounter information. Skipping." << std::endl;
       }
       h1_lumi = (TH1F*)infile->Get("lumi");
       if( h1_lumi!=0 ) {
         totalLumi += h1_lumi->GetBinContent(1);
         std::cout << "\tTotal lumi: " << totalLumi << " ub-1" << std::endl;
       } else {
-        std::cout << " WARNING! File '" << rootfilename << "' has no lumi information. Skipping." << std::endl;
+        std::cout << std::endl << " WARNING! File '" << rootfilename << "' has no lumi information. Skipping." << std::endl;
       }
       infile->Close();
 
@@ -152,20 +162,49 @@ float getWeight( const std::string& dataset, int nEvents ) {
 
   float xSection = -1.;
 
+  // all cross sections in pb-1:
   if( dataset=="ZJets_madgraph" ) {
-    xSection = 2800.;
+    xSection = 3048.; //NNLO see https://twiki.cern.ch/twiki/pub/CMS/GeneratorMain/ShortXsec.pdf
+  } else if( dataset=="Z1Jets_Pt0to100-alpgen_Spring10" ) {
+    xSection = 870.*0.447 ; // sigma x filter efficiency
+  } else if( dataset=="Z1Jets_Pt100to300-alpgen_Spring10" ) {
+    xSection = 19.3*0.504; // sigma x filter efficiency
+  } else if( dataset=="Z1Jets_Pt300to800-alpgen_Spring10" ) {
+    xSection = 0.226*0.352; // sigma x filter efficiency
+  } else if( dataset=="Z1Jets_Pt800to1600-alpgen_Spring10" ) {
+    xSection = 0.000528*0.266; // sigma x filter efficiency
+  } else if( dataset=="Z2Jets_Pt0to100-alpgen_Spring10" ) {
+    xSection = 372.*0.26; // sigma x filter efficiency
+  } else if( dataset=="Z2Jets_Pt100to300-alpgen_Spring10" ) {
+    xSection = 27.0*0.315; // sigma x filter efficiency
+  } else if( dataset=="Z2Jets_Pt300to800-alpgen_Spring10" ) {
+    xSection = 0.457*0.244; // sigma x filter efficiency
+  } else if( dataset=="Z2Jets_Pt800to1600-alpgen_Spring10" ) {
+    xSection = 0.00132*0.203; // sigma x filter efficiency
+  } else if( dataset=="Z3Jets_Pt0to100-alpgen_Spring10" ) {
+    xSection = 140.*0.157; // sigma x filter efficiency
+  } else if( dataset=="Z3Jets_Pt100to300-alpgen_Spring10" ) {
+    xSection = 20.3*0.189; // sigma x filter efficiency
+  } else if( dataset=="Z3Jets_Pt300to800-alpgen_Spring10" ) {
+    xSection = 0.465*0.162; // sigma x filter efficiency
+  } else if( dataset=="Z3Jets_Pt800to1600-alpgen_Spring10" ) {
+    xSection = 0.00152*0.149; // sigma x filter efficiency
   } else if( dataset=="HZZ_qqll_gluonfusion_M130" ) {
-    xSection = 25.560*0.03913*0.0337*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+    xSection = 25.560*0.03913*0.0674*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
   } else if( dataset=="HZZ_qqll_gluonfusion_M150" ) {
-    xSection = 19.568*0.08234*0.0337*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+    xSection = 19.568*0.08234*0.0674*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
   } else if( dataset=="HZZ_qqll_gluonfusion_M200" ) {
-    xSection = 10.361*0.2537*0.0337*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+    xSection = 10.361*0.2537*0.0674*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
   } else if( dataset=="HZZ_qqll_gluonfusion_M300" ) {
-    xSection = 5.2728*0.3053*0.0337*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+    xSection = 5.2728*0.3053*0.0674*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
   } else if( dataset=="HZZ_qqll_gluonfusion_M400" ) {
-    xSection = 4.8236*0.2664*0.0337*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+    xSection = 4.8236*0.2664*0.0674*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
   } else if( dataset=="HZZ_qqll_gluonfusion_M500" ) {
-    xSection = 2.1914*0.2602*0.0337*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+    xSection = 2.1914*0.2602*0.0674*0.7*2.; //sigma x BR(H->ZZ) x BR(Z->ll) x BR(Z->jj) x 2
+  } else if( dataset=="TTbar_2l_Spring10" ) {
+    xSection = 157.4*0.1080*2.; //NLO x BR(W->lnu) see https://twiki.cern.ch/twiki/pub/CMS/GeneratorMain/ShortXsec.pdf
+  } else if( dataset=="ZZ_Spring10" ) {
+    xSection = 5.9; //MCFM NLO see http://ceballos.web.cern.ch/ceballos/hwwlnln/cross_sections_backgrounds.txt
   } else {
     std::cout << std::endl;
     std::cout << "-> WARNING!! Dataset: '" << dataset << "' not present in database. Cross section unknown." << std::endl;
@@ -173,7 +212,18 @@ float getWeight( const std::string& dataset, int nEvents ) {
     return 1.;
   }
 
+  TString dataset_tstr(dataset.c_str());
+  TRegexp re("Z*Jets*alpgen");
+  if( dataset_tstr.Contains(re) ) {
+    std::cout << "-> Scaling LO alpgen cross-section to NNLO." << std::endl;
+    xSection*=(3048./2054.);
+  }
+
+
+  std::cout << "-> Total Events Analyzed: " << nEvents << ". " << std::endl;
+  std::cout << "-> Dataset cross-section: " << xSection << " pb" << std::endl;
   float weight = xSection/((float)nEvents);
+  std::cout << "=> Event weight: " << weight << std::endl;
 
   return weight;
 
