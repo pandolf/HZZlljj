9c7e276ab884577d448d0e5504ba299bca81c302
pandolf <>
2010-11-11 11:00:10 +0000

    correct version, supports batch

--- HZZlljjAnalyzer/test/analysis/do2ndLevel_TMVA.cpp
+++ HZZlljjAnalyzer/test/analysis/do2ndLevel_TMVA.cpp
@@ -5,16 +5,37 @@
 
 int main( int argc, char* argv[]) {
 
-  if( argc < 2 ) {
-    std::cout << "USAGE: ./do2ndLevel_TMVA [dataset]" << std::endl;
+  if( argc!=2 && argc!=3 && argc!=4 ) {
+    std::cout << "USAGE: ./do2ndLevel_TMVA [dataset] [inputfile=""] [flags=""]" << std::endl;
     exit(31);
   }
 
   std::string dataset(argv[1]);
 
-  Ntp1Analyzer_TMVA* na = new Ntp1Analyzer_TMVA(dataset);
+  Ntp1Analyzer_TMVA* na;
 
-  na->LoadInput();
+  std::string flags = "";
+  if( argc<4 ) {
+    na = new Ntp1Analyzer_TMVA(dataset, "BESTZ");
+  } else {
+    std::string flags_str(argv[3]);
+    flags = flags_str;
+    na = new Ntp1Analyzer_TMVA(dataset, "BESTZ", flags);
+  }
+
+
+//na->AddRequiredTrigger( "HLT_Ele15_LW_L1R" );
+//na->AddRequiredTrigger( "HLT_Mu9" );
+//na->AddRequiredTrigger( "HLT_Mu15" );
+
+
+  if( argc==2 ) {
+    na->LoadInput();
+  } else {
+    std::string inputfile(argv[2]);
+    na->LoadInputFromFile(inputfile.c_str());
+  }
+
   na->Loop();
 
   delete na;
