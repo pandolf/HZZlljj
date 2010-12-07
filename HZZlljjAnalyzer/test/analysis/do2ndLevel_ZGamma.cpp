199aa32b664b3d7424dba50bf279193bb423ca1e
pandolf <>
2010-12-07 11:26:20 +0000

    batch compatible

--- HZZlljjAnalyzer/test/analysis/do2ndLevel_ZGamma.cpp
+++ HZZlljjAnalyzer/test/analysis/do2ndLevel_ZGamma.cpp
@@ -5,16 +5,30 @@
 
 int main( int argc, char* argv[]) {
 
-  if( argc < 2 ) {
-    std::cout << "USAGE: ./do2ndLevel_ZGamma [dataset]" << std::endl;
+  if( argc!=2 && argc!=3 && argc!=4 ) {
+    std::cout << "USAGE: ./do2ndLevel_ZGamma [dataset] [inputfile=""] [flags=""]" << std::endl;
     exit(31);
   }
 
   std::string dataset(argv[1]);
 
-  Ntp1Analyzer_ZGamma* na = new Ntp1Analyzer_ZGamma(dataset);
+  Ntp1Analyzer_ZGamma* na;
 
-  na->LoadInput();
+  if( argc<4 ) {
+    na = new Ntp1Analyzer_ZGamma(dataset);
+  } else {
+    std::string flags(argv[3]);
+    na = new Ntp1Analyzer_ZGamma(dataset, flags);
+  }
+
+
+  if( argc==2 ) {
+    na->LoadInput();
+  } else {
+    std::string inputfile(argv[2]);
+    na->LoadInputFromFile(inputfile.c_str());
+  }
+
   na->Loop();
 
   delete na;
