feaa5a3d3821d13225185538f63e4a34c3a02218
pandolf <>
2011-03-11 09:10:32 +0000

    removed mH=600

--- HZZlljjAnalyzer/test/analysis/merge_and_setWeights_SCRIPT.sh
+++ HZZlljjAnalyzer/test/analysis/merge_and_setWeights_SCRIPT.sh
@@ -1,30 +1,55 @@
-./merge_and_setWeights HZZ_qqll_gluonfusion_M200
-./merge_and_setWeights HZZ_qqll_gluonfusion_M130
-./merge_and_setWeights HZZ_qqll_gluonfusion_M150
-./merge_and_setWeights HZZ_qqll_gluonfusion_M300
-./merge_and_setWeights HZZ_qqll_gluonfusion_M400
-./merge_and_setWeights HZZ_qqll_gluonfusion_M500
-./merge_and_setWeights ZJets_madgraph
-./merge_and_setWeights Z0Jets_Pt0to100-alpgen_Spring10
-./merge_and_setWeights Z1Jets_Pt0to100-alpgen_Spring10
-./merge_and_setWeights Z1Jets_Pt100to300-alpgen_Spring10
-./merge_and_setWeights Z1Jets_Pt300to800-alpgen_Spring10
-./merge_and_setWeights Z1Jets_Pt800to1600-alpgen_Spring10
-./merge_and_setWeights Z2Jets_Pt0to100-alpgen_Spring10
-./merge_and_setWeights Z2Jets_Pt100to300-alpgen_Spring10
-./merge_and_setWeights Z2Jets_Pt300to800-alpgen_Spring10
-./merge_and_setWeights Z2Jets_Pt800to1600-alpgen_Spring10
-./merge_and_setWeights Z3Jets_Pt0to100-alpgen_Spring10
-./merge_and_setWeights Z3Jets_Pt100to300-alpgen_Spring10
-./merge_and_setWeights Z3Jets_Pt300to800-alpgen_Spring10
-./merge_and_setWeights Z3Jets_Pt800to1600-alpgen_Spring10
-./merge_and_setWeights Z4Jets_Pt0to100-alpgen_Spring10
-./merge_and_setWeights Z4Jets_Pt100to300-alpgen_Spring10
-./merge_and_setWeights Z4Jets_Pt300to800-alpgen_Spring10
-./merge_and_setWeights Z4Jets_Pt800to1600-alpgen_Spring10
-./merge_and_setWeights Z5Jets_Pt0to100-alpgen_Spring10
-./merge_and_setWeights Z5Jets_Pt100to300-alpgen_Spring10
-./merge_and_setWeights Z5Jets_Pt300to800-alpgen_Spring10
-./merge_and_setWeights Z5Jets_Pt800to1600-alpgen_Spring10
-./merge_and_setWeights TTbar_2l_Spring10
-./merge_and_setWeights ZZ_Spring10
+  ./merge_and_setWeights Z0Jets_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z1Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z1Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z1Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z2Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z2Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z2Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z2Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z3Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z3Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z3Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z3Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z4Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z4Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z4Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z4Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z5Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z5Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z5Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola $1
+  ./merge_and_setWeights Z5Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola $1
+# ./merge_and_setWeights Z0Jets_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z1Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z1Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z1Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z2Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z2Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z2Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z2Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z3Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z3Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z3Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z3Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z4Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z4Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z4Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z4Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z5Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z5Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z5Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_v2 $1
+# ./merge_and_setWeights Z5Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-200_7TeV-jhu-pythia6 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-250_7TeV-jhu-pythia6 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-300_7TeV-jhu-pythia6 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-350_7TeV-jhu-pythia6 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-400_7TeV-jhu-pythia6 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-450_7TeV-jhu-pythia6 $1
+./merge_and_setWeights SMHiggsToZZTo2L2Q_M-500_7TeV-jhu-pythia6 $1
+./merge_and_setWeights ZZtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 $1
+./merge_and_setWeights WZtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 $1
+./merge_and_setWeights WWtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 $1
+./merge_and_setWeights TTJets_TuneZ2_7TeV-madgraph-tauola_Fall10 $1
+./merge_and_setWeights Mu_Nov4ReReco_PU $1
+./merge_and_setWeights Electron_Nov4ReReco_PU $1
