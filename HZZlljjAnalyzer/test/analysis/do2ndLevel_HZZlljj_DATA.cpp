ff68bd8f96e7351541a892406cab5a6c46cba859
pandolf <>
2011-06-21 12:27:38 +0000

    commented triggers and new JSON

--- HZZlljjAnalyzer/test/analysis/do2ndLevel_HZZlljj_DATA.cpp
+++ HZZlljjAnalyzer/test/analysis/do2ndLevel_HZZlljj_DATA.cpp
@@ -6,7 +6,7 @@
 int main( int argc, char* argv[]) {
 
   if( argc!=2 && argc!=3 && argc!=4 ) {
-    std::cout << "USAGE: ./do2ndLevel_Hzzlljj [dataset] [inputfile=""] [flags=""]" << std::endl;
+    std::cout << "USAGE: ./do2ndLevel_HZZlljj [dataset] [inputfile=""] [flags=""]" << std::endl;
     exit(31);
   }
 
@@ -21,19 +21,24 @@ int main( int argc, char* argv[]) {
     na = new Ntp1Analyzer_HZZlljj(dataset, flags);
   }
 
-  na->AddRequiredTrigger( "HLT_Mu9" );
-  na->AddRequiredTrigger( "HLT_Mu15_v1" );
-  na->AddRequiredTrigger( "HLT_Ele10_LW_L1R" );
-  na->AddRequiredTrigger( "HLT_Ele15_LW_L1R" );
-  na->AddRequiredTrigger( "HLT_Ele15_SW_L1R" );
-  na->AddRequiredTrigger( "HLT_Ele15_SW_CaloEleId_L1R" );
-  na->AddRequiredTrigger( "HLT_Ele17_SW_CaloEleId_L1R" );
-  na->AddRequiredTrigger( "HLT_Ele17_SW_TightEleId_L1R" );
-  na->AddRequiredTrigger( "HLT_Ele17_SW_TighterEleIdIsol_L1R_v2" );
-  na->AddRequiredTrigger( "HLT_Ele17_SW_TighterEleIdIsol_L1R_v3" );
+  //na->AddRequiredTrigger( "HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v2" );
+  //na->AddRequiredTrigger( "HLT_DoubleMu7_v1" );
 
-  //na->ReadJSONFile("Cert_132440-149442_7TeV_StreamExpress_Collisions10_CMSSWConfig_v3.txt");
-  na->ReadJSONFile("Cert_136033-149442_7TeV_Nov4ReReco_Collisions10_CMSSWConfig.txt");
+//na->AddRequiredTrigger( "HLT_Mu9" );
+//na->AddRequiredTrigger( "HLT_Mu15_v1" );
+//na->AddRequiredTrigger( "HLT_Ele10_LW_L1R" );
+//na->AddRequiredTrigger( "HLT_Ele15_LW_L1R" );
+//na->AddRequiredTrigger( "HLT_Ele15_SW_L1R" );
+//na->AddRequiredTrigger( "HLT_Ele15_SW_CaloEleId_L1R" );
+//na->AddRequiredTrigger( "HLT_Ele17_SW_CaloEleId_L1R" );
+//na->AddRequiredTrigger( "HLT_Ele17_SW_TightEleId_L1R" );
+//na->AddRequiredTrigger( "HLT_Ele17_SW_TighterEleIdIsol_L1R_v2" );
+//na->AddRequiredTrigger( "HLT_Ele17_SW_TighterEleIdIsol_L1R_v3" );
+
+  //na->ReadJSONFile("Cert_160404-163369_7TeV_PromptReco_Collisions11_CMSSWConfig.txt");
+  //na->ReadJSONFile("Cert_160404-163757_7TeV_PromptReco_Collisions11_CMSSWConfig.txt");
+  //na->ReadJSONFile("Cert_160404-165121_7TeV_PromptReco_Collisions11_CMSSWConfig.txt");
+  na->ReadJSONFile("Cert_160404-165542_7TeV_PromptReco_Collisions11_CMSSWConfig.txt");
 
   if( argc==2 ) {
     na->LoadInput();
