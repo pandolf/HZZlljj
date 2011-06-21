01f461999b2b769e7f12ea5976ac38230ba25f6f
pandolf <>
2011-06-21 16:22:59 +0000

    Spring11_v2

--- HZZlljjAnalyzer/test/analysis/finalize_all.sh
+++ HZZlljjAnalyzer/test/analysis/finalize_all.sh
@@ -4,19 +4,21 @@
 # --------------------------------------------------
 
 if( $2 == "ELE" ) then
-./finalize_HZZlljj Electron_Nov4ReReco_PU $1 $2
+./finalize_HZZlljj DoubleMu_Run2011A $1 $2
 else if( $2 == "MU" ) then
-./finalize_HZZlljj Mu_Nov4ReReco_PU $1 $2
+./finalize_HZZlljj DoubleElectron_Run2011A $1 $2
 else
-./finalize_HZZlljj EleMu_Nov4ReReco_PU $1 $2
+./finalize_HZZlljj DATA_Run2011A $1 $2
 endif
-./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-250_7TeV-jhu-pythia6 $1 $2
-./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-300_7TeV-jhu-pythia6 $1 $2
-./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-350_7TeV-jhu-pythia6 $1 $2
-./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-400_7TeV-jhu-pythia6 $1 $2
-./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-450_7TeV-jhu-pythia6 $1 $2
-./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-500_7TeV-jhu-pythia6 $1 $2
-./finalize_HZZlljj VVtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 $1 $2
-./finalize_HZZlljj TTJets_TuneZ2_7TeV-madgraph-tauola_Fall10 $1 $2
-#./finalize_HZZlljj ZJets_madgraph $1
-./finalize_HZZlljj ZJets_alpgen_TuneZ2_Fall10 $1 $2
+./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-250_7TeV-jhu-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-300_7TeV-jhu-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-350_7TeV-jhu-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-400_7TeV-jhu-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-450_7TeV-jhu-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj SMHiggsToZZTo2L2Q_M-500_7TeV-jhu-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj GluGluToHToZZTo2L2Q_M-600_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 $1 $2
+./finalize_HZZlljj VVtoAnything_TuneZ2_7TeV-pythia6-tauola_Spring11_v2 $1 $2
+./finalize_HZZlljj TT_TW_TuneZ2_7TeV-pythia6-tauola_Spring11_v2 $1 $2
+./finalize_HZZlljj ZJets_alpgen_TuneZ2_Spring11_v2 $1 $2
+./finalize_HZZlljj ZCC_alpgen_TuneZ2_Spring11_v2 $1 $2
+./finalize_HZZlljj ZBB_alpgen_TuneZ2_Spring11_v2 $1 $2
