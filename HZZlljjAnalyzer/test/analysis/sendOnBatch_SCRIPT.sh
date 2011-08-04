a8df8ffd001e5721f459b3c44bfae96c8aedde0b
pandolf <>
2011-08-04 08:41:41 +0000

    working version for all datasets used at EPS

--- HZZlljjAnalyzer/test/analysis/sendOnBatch_SCRIPT.sh
+++ HZZlljjAnalyzer/test/analysis/sendOnBatch_SCRIPT.sh
@@ -1,26 +1,76 @@
 # usage: source sendOnBatch_SCRIPT.sh [analyzerType="HZZlljj"]
 
-   python sendOnBatch.py Z0Jets_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z2Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z3Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z4Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z5Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z1Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z2Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z3Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z4Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z5Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z1Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z2Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z3Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z4Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z5Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z1Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z2Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z3Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z4Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
-  python sendOnBatch.py Z5Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-190_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-200_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-210_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-230_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-300_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-325_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-425_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-525_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-550_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-575_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+python sendOnBatch.py GluGluToHToZZTo2L2Q_M-600_7TeV-powheg-pythia6_Summer11-PU_S4_START42_V11-v1 2 $1
+
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-200_7TeV-jhu-pythia6 2 $1
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-250_7TeV-jhu-pythia6 2 $1
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-300_7TeV-jhu-pythia6 2 $1
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-350_7TeV-jhu-pythia6 2 $1
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-400_7TeV-jhu-pythia6 2 $1
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-450_7TeV-jhu-pythia6 2 $1
+ python sendOnBatch.py SMHiggsToZZTo2L2Q_M-500_7TeV-jhu-pythia6 2 $1
+
+ python sendOnBatch.py ZZtoAnything_TuneZ2_7TeV-pythia6-tauola_Spring11_v2 2 $1
+ python sendOnBatch.py WWtoAnything_TuneZ2_7TeV-pythia6-tauola_Spring11_v2 2 $1
+ python sendOnBatch.py WZtoAnything_TuneZ2_7TeV-pythia6-tauola_Spring11_v2 2 $1
+ python sendOnBatch.py TToBLNu_TuneZ2_tW-channel_7TeV-madgraph_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 2 $1
+ python sendOnBatch.py TTTo2L2Nu2B_7TeV-powheg-pythia6_Spring11-PU_S1_START311_V1G1-v1_Spring11_v2 2 $1
+ python sendOnBatch.py DYJetsToLL_TuneZ2_M-50_7TeV-madgraph-tauola_Summer11-PU_S4_START42_V11-v1 5 $1
+
+#python sendOnBatch.py Z0Jets_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z1Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z1Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z1Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z2Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z2Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z2Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1_2 2 $1
+#python sendOnBatch.py Z2Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z3Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z3Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z3Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z3Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z4Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z4Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z4Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z4Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z5Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z5Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z5Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+#python sendOnBatch.py Z5Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_Spring11-PU_S1_START311_V1G1-v1 2 $1
+
+#  python sendOnBatch.py Z0Jets_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z2Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z3Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z4Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z5Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z1Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z2Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z3Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z4Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z5Jets_ptZ-100to300_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z1Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z2Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z3Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z4Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z5Jets_ptZ-300to800_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z1Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z2Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z3Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z4Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+# python sendOnBatch.py Z5Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_3 2 $1
+
 
 # python sendOnBatch.py Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_v2_2 2 $1
 # python sendOnBatch.py Z2Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola_3 2 $1
@@ -42,14 +92,3 @@
 # python sendOnBatch.py Z3Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2_2 2 $1
 # python sendOnBatch.py Z4Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2_2 2 $1
 # python sendOnBatch.py Z5Jets_ptZ-800to1600_TuneZ2_7TeV-alpgen-tauola_v2_2 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-200_7TeV-jhu-pythia6 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-250_7TeV-jhu-pythia6 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-300_7TeV-jhu-pythia6 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-350_7TeV-jhu-pythia6 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-400_7TeV-jhu-pythia6 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-450_7TeV-jhu-pythia6 2 $1
-  python sendOnBatch.py SMHiggsToZZTo2L2Q_M-500_7TeV-jhu-pythia6 2 $1
- #python sendOnBatch.py TTJets_TuneZ2_7TeV-madgraph-tauola_Fall10 2 $1
- #python sendOnBatch.py ZZtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 2 $1
- #python sendOnBatch.py WZtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 2 $1
- #python sendOnBatch.py WWtoAnything_TuneZ2_7TeV-pythia6-tauola_Fall10 2 $1
