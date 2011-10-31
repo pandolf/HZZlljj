db0c4ee30978751ccf235accb3b64291b02c68f8
pandolf <>
2011-10-31 13:05:20 +0000

    now 35 bins

--- HZZlljjAnalyzer/test/analysis/PUWeight.C
+++ HZZlljjAnalyzer/test/analysis/PUWeight.C
@@ -36,7 +36,6 @@ PUWeight::PUWeight(float luminosity, const char* year, const std::string& idealM
   if (!LoadDataHistogram(luminosity, year))
     return;
 
-std::cout << "idealMCPU: " << idealMCPU << std::endl;
   //No MC given. Take ideal MC
   IdealMCHistogram(idealMCPU);
 
@@ -135,7 +134,18 @@ void PUWeight::SetDataHistogram(const TH1F* dataHisto) {
   cout << ">> Switching to new data histogram: " << dataHisto->GetName()
        << endl;
 #endif
-  fData = (TH1F*)dataHisto->Clone("PU_Data");
+
+  //Read data histogram...
+  TH1F* fData_tmp = (TH1F*)dataHisto->Clone("PU_Data_tmp");
+
+  if( fData_tmp->GetXaxis()->GetNbins()==36 ) {
+    if( fData!=0 ) delete fData;
+    fData = new TH1F("PU_Data", "", 36, -0.5, 35.5);
+    for( unsigned iBin=1; iBin<37; ++iBin ) 
+      fData->SetBinContent( iBin, fData_tmp->GetBinContent(iBin) );
+  } else {
+    fData = (TH1F*)fData_tmp->Clone("PU_Data");
+  }
 
   if (fData->Integral() != 1) {
     cout << "NOTE [PUWeight]: Data histogram is not normalized to 1! Normalizing..."
@@ -197,8 +207,8 @@ TH1F* PUWeight::LoadDataHistogram(float luminosity, const char* year) {
     return 0;
   }
 
-  if( fData_tmp->GetXaxis()->GetNbins()==26 ) {
-    fData = new TH1F("PU_Data", "", 25, -0.5, 24.5);
+  if( fData_tmp->GetXaxis()->GetNbins()==36 ) {
+    fData = new TH1F("PU_Data", "", 35, -0.5, 34.5);
     for( unsigned iBin=1; iBin<26; ++iBin ) 
       fData->SetBinContent( iBin, fData_tmp->GetBinContent(iBin) );
   } else {
@@ -246,9 +256,9 @@ prova->Close();
 }
 
 TH1F* PUWeight::IdealMCHistogram( const std::string& puType) {
-  unsigned int nbins = 25;
+  unsigned int nbins = 36;
   float xmin = -0.5;
-  float xmax = 24.5;
+  float xmax = 35.5;
 
 //if (fData) {
 //  nbins = fData->GetXaxis()->GetNbins();
@@ -271,7 +281,7 @@ TH1F* PUWeight::IdealMCHistogram( const std::string& puType) {
 		     4.58337152809607E-05};
 */
 
-  float   idealpu[25];
+  float   idealpu[36];
 
   if( puType=="Spring11_Flat10" ) {
 
@@ -300,34 +310,82 @@ TH1F* PUWeight::IdealMCHistogram( const std::string& puType) {
       idealpu[22]   =   0.0002229748;
       idealpu[23]   =   0.0001028162;
       idealpu[24]   =   4.58337152809607E-05;
+      idealpu[25]   =   0.;
+      idealpu[26]   =   0.;
+      idealpu[27]   =   0.;
+      idealpu[28]   =   0.;
+      idealpu[29]   =   0.;
+      idealpu[30]   =   0.;
+      idealpu[31]   =   0.;
+      idealpu[32]   =   0.;
+      idealpu[33]   =   0.;
+      idealpu[34]   =   0.;
+      idealpu[35]   =   0.;
 
   } else if( puType=="Summer11_S4" ) {
 
-      idealpu[0] =  0.14551;
-      idealpu[1] =  0.0644453;
-      idealpu[2] =  0.0696412;
-      idealpu[3] =  0.0700311;
-      idealpu[4] =  0.0694257;
-      idealpu[5] =  0.0685655;
-      idealpu[6] =  0.0670929;
-      idealpu[7] =  0.0646049;
-      idealpu[8] =  0.0609383;
-      idealpu[9] =  0.0564597;
-      idealpu[10] =  0.0508014;
-      idealpu[11] =  0.0445226;
-      idealpu[12] =  0.0378796;
-      idealpu[13] =  0.0314746;
-      idealpu[14] =  0.0254139;
-      idealpu[15] =  0.0200091;
-      idealpu[16] =  0.0154191;
-      idealpu[17] =  0.0116242;
-      idealpu[18] =  0.00846857;
-      idealpu[19] =  0.00614328;
-      idealpu[20] =  0.00426355;
-      idealpu[21] =  0.00300632;
-      idealpu[22] =  0.00203485;
-      idealpu[23] =  0.00133045;
-      idealpu[24] =  0.000893794;
+      idealpu[0] = 1.45346E-01;
+      idealpu[1] = 6.42802E-02;
+      idealpu[2] = 6.95255E-02;
+      idealpu[3] = 6.96747E-02;
+      idealpu[4] = 6.92955E-02;
+      idealpu[5] = 6.84997E-02;
+      idealpu[6] = 6.69528E-02;
+      idealpu[7] = 6.45515E-02;
+      idealpu[8] = 6.09865E-02;
+      idealpu[9] = 5.63323E-02;
+      idealpu[10] = 5.07322E-02;
+      idealpu[11] = 4.44681E-02;
+      idealpu[12] = 3.79205E-02;
+      idealpu[13] = 3.15131E-02;
+      idealpu[14] = 2.54220E-02;
+      idealpu[15] = 2.00184E-02;
+      idealpu[16] = 1.53776E-02;
+      idealpu[17] = 1.15387E-02;
+      idealpu[18] = 8.47608E-03;
+      idealpu[19] = 6.08715E-03;
+      idealpu[20] = 4.28255E-03;
+      idealpu[21] = 2.97185E-03;
+      idealpu[22] = 2.01918E-03;
+      idealpu[23] = 1.34490E-03;
+      idealpu[24] = 8.81587E-04;
+      idealpu[25] = 5.69954E-04;
+      idealpu[26] = 3.61493E-04;
+      idealpu[27] = 2.28692E-04;
+      idealpu[28] = 1.40791E-04;
+      idealpu[29] = 8.44606E-05;
+      idealpu[30] = 5.10204E-05;
+      idealpu[31] = 3.07802E-05;
+      idealpu[32] = 1.81401E-05;
+      idealpu[33] = 1.00201E-05;
+      idealpu[34] = 5.80004E-06;
+      idealpu[35] = 0.;
+
+      //idealpu[0] =  0.14551;
+      //idealpu[1] =  0.0644453;
+      //idealpu[2] =  0.0696412;
+      //idealpu[3] =  0.0700311;
+      //idealpu[4] =  0.0694257;
+      //idealpu[5] =  0.0685655;
+      //idealpu[6] =  0.0670929;
+      //idealpu[7] =  0.0646049;
+      //idealpu[8] =  0.0609383;
+      //idealpu[9] =  0.0564597;
+      //idealpu[10] =  0.0508014;
+      //idealpu[11] =  0.0445226;
+      //idealpu[12] =  0.0378796;
+      //idealpu[13] =  0.0314746;
+      //idealpu[14] =  0.0254139;
+      //idealpu[15] =  0.0200091;
+      //idealpu[16] =  0.0154191;
+      //idealpu[17] =  0.0116242;
+      //idealpu[18] =  0.00846857;
+      //idealpu[19] =  0.00614328;
+      //idealpu[20] =  0.00426355;
+      //idealpu[21] =  0.00300632;
+      //idealpu[22] =  0.00203485;
+      //idealpu[23] =  0.00133045;
+      //idealpu[24] =  0.000893794;
 
   } else if( puType=="Summer11_S4_ave" ) {
 
@@ -356,44 +414,66 @@ TH1F* PUWeight::IdealMCHistogram( const std::string& puType) {
       idealpu[22] =  0.00070592;
       idealpu[23] =  0.000384744;
       idealpu[24] =  0.000219377;
+      idealpu[25]   =   0.;
+      idealpu[26]   =   0.;
+      idealpu[27]   =   0.;
+      idealpu[28]   =   0.;
+      idealpu[29]   =   0.;
+      idealpu[30]   =   0.;
+      idealpu[31]   =   0.;
+      idealpu[32]   =   0.;
+      idealpu[33]   =   0.;
+      idealpu[34]   =   0.;
+      idealpu[35]   =   0.;
 
   } else {
 
     std::cout << std::endl << "-----> WARNING!!!!!!!!!!" << std::endl;
-    std::cout << "Unknown puType: '" << puType << "'. Using Spring11_Flat10 default." << std::endl;
-      idealpu[0]  =  0.0698146584;
-      idealpu[1]   =   0.0698146584;
-      idealpu[2]   =   0.0698146584;
-      idealpu[3]   =   0.0698146584;
-      idealpu[4]   =   0.0698146584;
-      idealpu[5]   =   0.0698146584;
-      idealpu[6]   =   0.0698146584;
-      idealpu[7]   =   0.0698146584;
-      idealpu[8]   =   0.0698146584;
-      idealpu[9]   =   0.0698146584;
-      idealpu[10]   =   0.0698146584;
-      idealpu[11]   =   0.0630151648;
-      idealpu[12]   =   0.0526654164;
-      idealpu[13]   =   0.0402754482;
-      idealpu[14]   =   0.0292988928;
-      idealpu[15]   =   0.0194384503;
-      idealpu[16]   =   0.0122016783;
-      idealpu[17]   =   0.007207042;
-      idealpu[18]   =   0.004003637;
-      idealpu[19]   =   0.0020278322;
-      idealpu[20]   =   0.0010739954;
-      idealpu[21]   =   0.0004595759;
-      idealpu[22]   =   0.0002229748;
-      idealpu[23]   =   0.0001028162;
-      idealpu[24]   =   4.58337152809607E-05;
+    std::cout << "Unknown puType: '" << puType << "'. Using Summer11 default." << std::endl;
+      idealpu[0] = 1.45346E-01;
+      idealpu[1] = 6.42802E-02;
+      idealpu[2] = 6.95255E-02;
+      idealpu[3] = 6.96747E-02;
+      idealpu[4] = 6.92955E-02;
+      idealpu[5] = 6.84997E-02;
+      idealpu[6] = 6.69528E-02;
+      idealpu[7] = 6.45515E-02;
+      idealpu[8] = 6.09865E-02;
+      idealpu[9] = 5.63323E-02;
+      idealpu[10] = 5.07322E-02;
+      idealpu[11] = 4.44681E-02;
+      idealpu[12] = 3.79205E-02;
+      idealpu[13] = 3.15131E-02;
+      idealpu[14] = 2.54220E-02;
+      idealpu[15] = 2.00184E-02;
+      idealpu[16] = 1.53776E-02;
+      idealpu[17] = 1.15387E-02;
+      idealpu[18] = 8.47608E-03;
+      idealpu[19] = 6.08715E-03;
+      idealpu[20] = 4.28255E-03;
+      idealpu[21] = 2.97185E-03;
+      idealpu[22] = 2.01918E-03;
+      idealpu[23] = 1.34490E-03;
+      idealpu[24] = 8.81587E-04;
+      idealpu[25] = 5.69954E-04;
+      idealpu[26] = 3.61493E-04;
+      idealpu[27] = 2.28692E-04;
+      idealpu[28] = 1.40791E-04;
+      idealpu[29] = 8.44606E-05;
+      idealpu[30] = 5.10204E-05;
+      idealpu[31] = 3.07802E-05;
+      idealpu[32] = 1.81401E-05;
+      idealpu[33] = 1.00201E-05;
+      idealpu[34] = 5.80004E-06;
+      idealpu[35] =   0.;
 
   }
 
   for (unsigned int i = 0; i < nbins; i++) {
-    if (i < 25)
+    if (i < nbins)
       fMC->Fill(i, idealpu[i]);
     else
-      fMC->Fill(i, 0);
+      fMC->Fill(i, idealpu[nbins-1]);
   }
   return fMC;
 }
