49a7f2514cf8ca4516f2fbdebd5582d41fa58118
pandolf <>
2011-11-06 20:05:21 +0000

    now draws correctly expected limit

--- HZZlljjAnalyzer/test/analysis/FIT/draw_golfcourse.cpp
+++ HZZlljjAnalyzer/test/analysis/FIT/draw_golfcourse.cpp
@@ -10,6 +10,7 @@ std::pair<TGraphAsymmErrors*,TGraphAsymmErrors*> get_expectedLimit( const std::s
 float getMedian( TH1D* h1 );
 float get_lowBound( TH1D* h1, float frac );
 float get_upBound( TH1D* h1, float frac );
+TPaveText* get_labelCMS( DrawBase* db );
 
 
 
@@ -25,26 +26,55 @@ int main( int argc, char* argv[] ) {
 
   TString data_dataset_tstr(data_dataset);
   std::string PUType = "Run2011A";
-  if( data_dataset=="HR11" )
+  float lumi = 2100.;
+  if( data_dataset=="HR11" ) {
     PUType = "HR11";
-  if( data_dataset_tstr.BeginsWith("Run2011B") )
+    lumi = 4200.;
+  } if( data_dataset_tstr.BeginsWith("Run2011B") ) {
     PUType = "Run2011B";
-
+  }
 
   DrawBase* db = new DrawBase("Golfcourse");
+  db->set_lumiNormalization(lumi);
+  TFile* file_data = TFile::Open("HZZlljjRM_DATA_Run2011A_FULL_optLD_looseBTags_v2_ALL.root"); //any file is good, got to fix this
+  db->add_dataFile( file_data, "dummydata" );
+
 
 
   std::pair<TGraphAsymmErrors*,TGraphAsymmErrors*> graphs_expected = get_expectedLimit( data_dataset, PUType );
   TGraphAsymmErrors* graphExpected68 = graphs_expected.first;
   TGraphAsymmErrors* graphExpected95 = graphs_expected.second;
-graphExpected68->Print();
-graphExpected95->Print();
 
+  graphExpected68->SetFillColor(kGreen);
+  graphExpected68->SetLineColor(kGreen+2);
+  graphExpected68->SetLineWidth(2);
+  graphExpected68->SetFillStyle(1001);//solid
+
+  graphExpected95->SetFillColor(kYellow);
+  graphExpected95->SetFillStyle(1001);//solid
+
+  TH2D* axes = new TH2D("axes", "", 10, 160., 640., 10, 0., 10.);
+  axes->SetXTitle("m_{H} [GeV]");
+  axes->SetYTitle("#sigma_{95%} / #sigma_{SM}");
+
+  //TPaveText* labelCMS = get_labelCMS(db);
+  TPaveText* labelCMS = db->get_labelCMS();
+  TPaveText* labelSqrt = db->get_labelSqrt();
+
+  TLine* line_one = new TLine( 160., 1., 640., 1.);
+  line_one->SetLineColor(kRed);
+  line_one->SetLineWidth(2);
 
   TCanvas* c1 = new TCanvas("c1", "", 600, 600);
   c1->cd();
-  graphExpected68->Draw("APE");
-  graphExpected95->Draw("APEsame");
+  axes->Draw();
+  labelCMS->Draw("same");
+  labelSqrt->Draw("same");
+  graphExpected95->Draw("3same");
+  graphExpected68->Draw("3same");
+  graphExpected68->Draw("LXsame");
+  line_one->Draw("same");
+  gPad->RedrawAxis();
   c1->SaveAs("Prova.eps");
 
   return 0;
@@ -62,8 +92,6 @@ std::pair<TGraphAsymmErrors*,TGraphAsymmErrors*> get_expectedLimit( const std::s
   TGraphAsymmErrors* graph95 = new TGraphAsymmErrors(0);
 
 
-
-  
   std::vector<int> masses;
   masses.push_back(200);
   masses.push_back(250);
@@ -167,3 +195,27 @@ float get_lowBound( TH1D* h1, float frac ) {
 
 }
 
+
+
+TPaveText* get_labelCMS( DrawBase* db ) {
+
+  float x1 = 0.145;
+  float y1 = 0.953;
+  float x2 = 0.6;
+  float y2 = 0.975;
+
+  
+  TPaveText* cmslabel = new TPaveText( x1, y1, x2, y2, "brNDC" );
+  cmslabel->SetFillColor(kWhite);
+  cmslabel->SetTextSize(0.038);
+  cmslabel->SetTextAlign(11);
+  cmslabel->SetTextSize(0.038);
+  cmslabel->SetTextFont(62);
+  std::string leftText = "CMS Preliminary 2011";
+  cmslabel->SetTextAlign(11); // align left
+  std::string lumiText = db->get_lumiText();
+  cmslabel->AddText(Form("%s, %s", leftText.c_str(), lumiText.c_str()));
+
+  return cmslabel;
+
+}
