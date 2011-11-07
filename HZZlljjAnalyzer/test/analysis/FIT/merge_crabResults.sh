e1494c2b6f2e1ad8d4fb8bd9b8f904bd48a137ce
pandolf <>
2011-11-07 16:58:04 +0000

    HR11

--- HZZlljjAnalyzer/test/analysis/FIT/merge_crabResults.sh
+++ HZZlljjAnalyzer/test/analysis/FIT/merge_crabResults.sh
@@ -1,4 +1,4 @@
-foreach i ( `ls -1 | grep "[0-9][0-9][0-9]_Run2011A_FULL_4" | grep -v r ` )
+foreach i ( `ls -1 | grep "[0-9][0-9][0-9]_HR11" | grep -v r ` )
 cd $i/res
 foreach j (`ls -1 outputToy*.tgz`)
 tar -xvf $j
