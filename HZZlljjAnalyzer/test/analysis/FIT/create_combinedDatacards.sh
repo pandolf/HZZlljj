d49647d76b9935e319e4b72d46604c8e90b4f7c2
pandolf <>
2011-11-09 16:40:20 +0000

    now combining also three categories separately

--- HZZlljjAnalyzer/test/analysis/FIT/create_combinedDatacards.sh
+++ HZZlljjAnalyzer/test/analysis/FIT/create_combinedDatacards.sh
@@ -12,6 +12,12 @@ cd $i
 echo "Combining datacards for mass: $i"
 combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b.${i}.txt CMS_hzz2l2q_ee1b=hzz2l2q_ee1b.${i}.txt CMS_hzz2l2q_ee2b=hzz2l2q_ee2b.${i}.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b.${i}.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b.${i}.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b.${i}.txt >! CMS_hzz2l2q_${i}_6channels.txt
 text2workspace.py CMS_hzz2l2q_${i}_6channels.txt -b -m $i -o model.root
+combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b.${i}.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b.${i}.txt >! CMS_hzz2l2q_${i}_0btag.txt
+text2workspace.py CMS_hzz2l2q_${i}_0btag.txt -b -m $i -o model_0btag.root
+combineCards.py CMS_hzz2l2q_ee1b=hzz2l2q_ee1b.${i}.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b.${i}.txt >! CMS_hzz2l2q_${i}_1btag.txt
+text2workspace.py CMS_hzz2l2q_${i}_1btag.txt -b -m $i -o model_1btag.root
+combineCards.py CMS_hzz2l2q_ee2b=hzz2l2q_ee2b.${i}.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b.${i}.txt >! CMS_hzz2l2q_${i}_2btag.txt
+text2workspace.py CMS_hzz2l2q_${i}_2btag.txt -b -m $i -o model_2btag.root
 cd ..
 end
 
