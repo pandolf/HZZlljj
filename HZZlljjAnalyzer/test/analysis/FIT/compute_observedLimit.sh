670565581347059c8164e3ffa8a163f0f04ce1bc
pandolf <>
2011-11-09 16:17:52 +0000

    endif

--- HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit.sh
+++ HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit.sh
@@ -1,11 +1,20 @@
-setenv putype $1
+setenv dataset $1
+setenv nbtags $2
 
-cd datacards_${putype}
+cd datacards_${dataset}
+
+
 
 foreach i ( `ls -1 | grep "[0-9][0-9][0-9]" | grep -v r ` )
+  #setenv i 500
   cd $i
   echo "Computing observed limit for mass: $i"
-  combine model.root -M MarkovChainMC -m $i -H ProfileLikelihood -U >&! log.txt
+  if ( $# > 1 ) then
+    combine model_${nbtags}btag.root -M MarkovChainMC -m $i -H ProfileLikelihood -U >&! log_${nbtags}btag.txt
+  else
+    combine model.root -M MarkovChainMC -m $i -H ProfileLikelihood -U >&! log.txt
+  endif
   cd -
+  #break
 end
 cd ..
