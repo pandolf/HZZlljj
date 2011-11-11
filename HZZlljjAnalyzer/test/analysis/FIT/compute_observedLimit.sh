ac77976e71d64dee739521d345d01d6c7631cf01
pandolf <>
2011-11-11 14:21:44 +0000

    now sourceing from masses.txt

--- HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit.sh
+++ HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit.sh
@@ -1,12 +1,10 @@
 setenv dataset $1
 setenv nbtags $2
 
-cd datacards_${dataset}
 
 
-
-foreach i ( `ls -1 | grep "[0-9][0-9][0-9]" | grep -v r ` )
-  cd $i
+foreach i ( `less masses.txt | grep "[0-9][0-9][0-9]" | grep -v r ` )
+  cd datacards_${dataset}/$i
   echo "Computing observed limit for mass: $i"
   if ( $# > 1 ) then
     combine model_${nbtags}btag.root -M MarkovChainMC -m $i -H ProfileLikelihood -U >&! log_${nbtags}btag.txt
--- HZZlljjAnalyzer/test/analysis/FIT/create_combinedDatacards.sh
+++ HZZlljjAnalyzer/test/analysis/FIT/create_combinedDatacards.sh
@@ -3,12 +3,10 @@
 
 setenv dataset $1
 
-cd datacards_${dataset}
 
-pwd
-
-foreach i ( `ls -1 | grep "[0-9][0-9][0-9]" | grep -v r ` )
-cd $i
+foreach i ( `less masses.txt | grep "[0-9][0-9][0-9]" | grep -v r ` )
+#foreach i ( `ls -1 | grep "[0-9][0-9][0-9]" | grep -v r ` )
+cd datacards_${dataset}/$i
 echo "Combining datacards for mass: $i"
 combineCards.py CMS_hzz2l2q_ee0b=hzz2l2q_ee0b.${i}.txt CMS_hzz2l2q_ee1b=hzz2l2q_ee1b.${i}.txt CMS_hzz2l2q_ee2b=hzz2l2q_ee2b.${i}.txt CMS_hzz2l2q_mm0b=hzz2l2q_mm0b.${i}.txt CMS_hzz2l2q_mm1b=hzz2l2q_mm1b.${i}.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b.${i}.txt >! CMS_hzz2l2q_${i}_6channels.txt
 text2workspace.py CMS_hzz2l2q_${i}_6channels.txt -b -m $i -o model.root
@@ -18,7 +16,7 @@ combineCards.py CMS_hzz2l2q_ee1b=hzz2l2q_ee1b.${i}.txt CMS_hzz2l2q_mm1b=hzz2l2q_
 text2workspace.py CMS_hzz2l2q_${i}_1btag.txt -b -m $i -o model_1btag.root
 combineCards.py CMS_hzz2l2q_ee2b=hzz2l2q_ee2b.${i}.txt CMS_hzz2l2q_mm2b=hzz2l2q_mm2b.${i}.txt >! CMS_hzz2l2q_${i}_2btag.txt
 text2workspace.py CMS_hzz2l2q_${i}_2btag.txt -b -m $i -o model_2btag.root
-cd ..
+cd -
 end
 
-cd ..
+#cd ..
