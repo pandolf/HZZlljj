1c2abec886b585d69c288a8ecd827b1e54d068f8
pandolf <>
2011-11-19 11:52:05 +0000

    proto-working version

--- HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit_batch.py
+++ HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit_batch.py
@@ -25,16 +25,17 @@ massesFile = open('masses.txt', 'r')
 
 for line in massesFile:
 
-  massDir = datacards_dir + "/" + str(line)
+  mass =  line.strip('\n')
+  massDir = datacards_dir + "/" + str(mass)
   scriptName = massDir + "/batchScript.src"
   diskoutputmain = '/cmsrm/pc18/pandolf/CMSSW_4_2_8/src/HZZlljj/HZZlljjAnalyzer/test/analysis/FIT/' + massDir
   scriptFile = open(scriptName,'w')
   scriptFile.write('#!/bin/bash\n')
   scriptFile.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
   scriptFile.write('cd /afs/cern.ch/user/p/pandolf/scratch1/CMSSW_4_2_3_patch5/ ; eval `scramv1 runtime -sh` ; cd -\n')
-  scriptFile.write('cp '+massDir+'/model*.root $WORKDIR\n')
+  scriptFile.write('cp '+pwd+"/"+massDir+'/model*.root $WORKDIR\n')
   scriptFile.write('cd $WORKDIR\n')
-  scriptFile.write('combine model.root -M MarkovChainMC -m ' + str(line) + '-H ProfileLikelihood -U >&! log.txt')
+  scriptFile.write('combine model.root -M MarkovChainMC -m ' + str(mass) + ' -H ProfileLikelihood -U >&! log.txt\n')
   scriptFile.write('ls log*.txt | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm22:'+diskoutputmain+'/{}\n') 
   scriptFile.close
   os.system("echo bsub -q "+queue+" -o "+pwd+"/log.log source "+pwd+"/"+scriptName)
