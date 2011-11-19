ffbbf491c35bdf963e32152c187ce9ce3df44af7
pandolf <>
2011-11-19 14:25:15 +0000

    added export C language files. working?

--- HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit_batch.py
+++ HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit_batch.py
@@ -32,9 +32,12 @@ for line in massesFile:
   scriptFile = open(scriptName,'w')
   scriptFile.write('#!/bin/bash\n')
   scriptFile.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
+  scriptFile.write('export LANGUAGE="C"\n')
+  scriptFile.write('export LC_ALL="C"\n')
   scriptFile.write('cd /afs/cern.ch/user/p/pandolf/scratch1/CMSSW_4_2_3_patch5/ ; eval `scramv1 runtime -sh` ; cd -\n')
   scriptFile.write('cp '+pwd+"/"+massDir+'/model*.root $WORKDIR\n')
   scriptFile.write('cd $WORKDIR\n')
+  scriptFile.write('echo "Computing upper limit for mass: ' + str(mass) + '\n')
   scriptFile.write('combine model.root -M MarkovChainMC -m ' + str(mass) + ' -H ProfileLikelihood -U >& log.txt\n')
   scriptFile.write('ls log*.txt | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm22:'+diskoutputmain+'/{}\n') 
   scriptFile.close
