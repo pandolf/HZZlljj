6a15a55ff5133e761ed3371e7056fb2caab71f3b
pandolf <>
2011-11-19 14:07:47 +0000

    removed pernicious exclamation mark

--- HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit_batch.py
+++ HZZlljjAnalyzer/test/analysis/FIT/compute_observedLimit_batch.py
@@ -20,7 +20,7 @@ pwd = os.environ['PWD']
 datacards_dir = "datacards_" + dataset
 
 
-massesFile = open('masses.txt', 'r')
+massesFile = open('masses_tmp.txt', 'r')
 
 
 for line in massesFile:
@@ -35,7 +35,7 @@ for line in massesFile:
   scriptFile.write('cd /afs/cern.ch/user/p/pandolf/scratch1/CMSSW_4_2_3_patch5/ ; eval `scramv1 runtime -sh` ; cd -\n')
   scriptFile.write('cp '+pwd+"/"+massDir+'/model*.root $WORKDIR\n')
   scriptFile.write('cd $WORKDIR\n')
-  scriptFile.write('combine model.root -M MarkovChainMC -m ' + str(mass) + ' -H ProfileLikelihood -U >&! log.txt\n')
+  scriptFile.write('combine model.root -M MarkovChainMC -m ' + str(mass) + ' -H ProfileLikelihood -U >& log.txt\n')
   scriptFile.write('ls log*.txt | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm22:'+diskoutputmain+'/{}\n') 
   scriptFile.close
   os.system("echo bsub -q "+queue+" -o "+pwd+"/log.log source "+pwd+"/"+scriptName)
