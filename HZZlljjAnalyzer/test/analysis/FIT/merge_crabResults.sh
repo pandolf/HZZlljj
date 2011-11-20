051e5d313dc9b00f44d8ae5723fb16dae7e1217a
pandolf <>
2011-11-20 11:37:25 +0000

    added fitDATA/fitMC

--- HZZlljjAnalyzer/test/analysis/FIT/merge_crabResults.sh
+++ HZZlljjAnalyzer/test/analysis/FIT/merge_crabResults.sh
@@ -1,6 +1,7 @@
 setenv dataset $1
+setenv data_mc $2
 
-cd UpperLimitToys_${dataset}
+cd UpperLimitToys_${dataset}_fit${data_mc}
 
 foreach i ( `ls -1 | grep "[0-9][0-9][0-9]" | grep -v r ` )
 cd $i/res
